3647cfdb42c9605245b037ac656eb80d362e2147
Mykhailo Herasimov <mogerasimov@gmail.com>
2020-02-10 22:18:06 -0400

    minor modifications

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -36,19 +36,19 @@ export class LandingComponent implements OnInit {
       new BlogItem('',
                         'The Perfect Holiday Cookie, Based on Your Zodiac Sign',
                       'Trevor Parker',
-                     'Just in case you haven’t noticed already, we’ve launched a new feature that enables all of our community members to upload their own recipes through Kitchen Stories’ app! Ever since the launch, we’ve been marveling at the creativity and diversity of these brilliant recipes, from sesame chicken, lentil …'),
+                     'Just in case you haven’t noticed already, we’ve launched a new feature that enables all of our community members to upload their own recipes through Kitchen Stories’ app! Ever since the launch, we’ve been marveling at the creativity and diversity of these brilliant recipes, from sesame chicken, lentil …', 0),
       new BlogItem('',
                         'Everything You Need to Know About Preparing and Storing In Season',
                       'Martha Dixon',
-                     'I guess you could say my childhood was strewn together with beautifully slivered oranges. Whether it was a birthday, wedding, Chinese New Year, or simply a fresh dessert on a weeknight, there was never an inappropriate time for my grandma to present bouquet of orange slices. Its favored place in ...'),
+                     'I guess you could say my childhood was strewn together with beautifully slivered oranges. Whether it was a birthday, wedding, Chinese New Year, or simply a fresh dessert on a weeknight, there was never an inappropriate time for my grandma to present bouquet of orange slices. Its favored place in ...', 0),
       new BlogItem('',
                         'Beat the Blues with This Winter Salad with Summery Hues',
                       'Jeffery Neal',
-                     'Just in case you haven’t noticed already, we’ve launched a new feature that enables all of our community members to upload their own recipes through Kitchen Stories’ app! Ever since the launch, we’ve been marveling at the creativity and diversity of these brilliant recipes, from sesame chicken, lentil …'),
+                     'Just in case you haven’t noticed already, we’ve launched a new feature that enables all of our community members to upload their own recipes through Kitchen Stories’ app! Ever since the launch, we’ve been marveling at the creativity and diversity of these brilliant recipes, from sesame chicken, lentil …', 0),
       new BlogItem('',
                         'Everything You Need to Know About Preparing and Storing In Season Oranges',
                       'Edna Schwartz',
-                     'I guess you could say my childhood was strewn together with beautifully slivered oranges. Whether it was a birthday, wedding, Chinese New Year, or simply a fresh dessert on a weeknight, there was never an inappropriate time for my grandma to present a bouquet of orange slices. Its favored place in ...')
+                     'I guess you could say my childhood was strewn together with beautifully slivered oranges. Whether it was a birthday, wedding, Chinese New Year, or simply a fresh dessert on a weeknight, there was never an inappropriate time for my grandma to present a bouquet of orange slices. Its favored place in ...',0)
     ]
   };
 
