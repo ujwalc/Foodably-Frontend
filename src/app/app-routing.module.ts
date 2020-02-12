932ac2aaca4f62555e00158abce376854220fe09
Raviteja Kase <rv270985@dal.ca>
2020-02-12 03:17:35 -0400

    added Recipes page and populated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
+import { RecipesComponent } from './recipes/recipes.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { BlogsComponent } from './blogs/blogs.component';
 
+
 const routes: Routes = [
   { path: '', component: LandingComponent, pathMatch: 'full' },
   { path: 'blogs', component: BlogsComponent },
+  {path:'recipes',component:RecipesComponent}
 ];
 
 @NgModule({
