798893be3a5a4cc59c487005b32fc1603d602d5a
Sneha <sneha.seraph@gmail.com>
2020-02-14 12:35:00 -0400

    created html file for profile component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { BlogsComponent } from './blogs/blogs.component';
+import {ProfileComponent} from './profile/profile.component'
 
 const routes: Routes = [
   { path: '', component: LandingComponent, pathMatch: 'full' },
   { path: 'blogs', component: BlogsComponent },
+  {path: 'profile',component:ProfileComponent}
 ];
 
 @NgModule({
