f383098df87a5d0911f64be1f736359583a7468e
Sneha <sneha.seraph@gmail.com>
2020-03-18 18:25:12 -0300

    sneha changes

--- src/app/view-recipe/comment-section/comment-section.component.ts
+++ src/app/view-recipe/comment-section/comment-section.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-comment-section',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comment-section.component.scss']
 })
 export class CommentSectionComponent implements OnInit {
-
-  constructor() { }
-
+  comment:String
+  
+  constructor(private httpClient: HttpClient) { }
+  
   ngOnInit() {
+  }
+
+  onComment(form: NgForm){
+    console.log(form.value);
+    const commentData={
+      comment:form.value
+    }
+    this.httpClient.post(`http://localhost:3000/comment`, commentData).toPromise()
+    .then()
+    .catch(err => console.log(err));
+    this.comment=form.value;
+    
+    form.reset();
+    return this.comment
   }
 
 }
