add080885bafb7f017fbce0d6a8216a580158ddd
Mykhailo Herasimov <mogerasimov@gmail.com>
2020-03-24 21:36:35 -0300

    added annotation information to the recipe

--- src/app/shared/models/recipe/recipe.model.ts
+++ src/app/shared/models/recipe/recipe.model.ts
@@ -4,9 +4,16 @@ import { Ingredient } from './ingredient.model';
 
 export class Recipe {
 
+  get annotations(): string[] {
+    return [this.type, this.cuisine, this.category];
+  }
+
   constructor(public imagePath: string,
               public title: string,
               public description: string,
+              public cuisine: string,
+              public category: string,
+              public type: string,
               public author: string,
               public createDate: string,
               public relatedRecipes: RecipeSection,
--- src/app/view-recipe/recipe-annotation/recipe-annotation.component.ts
+++ src/app/view-recipe/recipe-annotation/recipe-annotation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-recipe-annotation',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeAnnotationComponent implements OnInit {
 
+  @Input()
+  hasBorder = true;
+
+  @Input()
+  annotation: string;
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
