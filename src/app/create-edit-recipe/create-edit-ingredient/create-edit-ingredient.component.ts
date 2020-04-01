4b8cc513f85491e376a94f1b2084f2b4f0d36844
Mykhailo Herasimov <mogerasimov@gmail.com>
2020-04-01 01:51:50 -0300

    added ingredient section

--- src/app/create-edit-recipe/create-edit-ingredient/create-edit-ingredient.component.ts
+++ src/app/create-edit-recipe/create-edit-ingredient/create-edit-ingredient.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-create-edit-ingredient',
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateEditIngredientComponent implements OnInit {
 
+  @Input()
+  isLast = true;
+
+  units = ['kg', 't. sp.', 'g'];
+
+  styleGuide = {
+    caretClass: 'dropdown__caret',
+    selectBoxClass: 'dropdown__content',
+    selectMenuClass: 'dropdown',
+    optionsClass: 'dropdown__option'
+  };
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onDelete() {
+
+  }
 }
--- src/app/shared/controls/button-square/button-square.component.ts
+++ src/app/shared/controls/button-square/button-square.component.ts
@@ -4,7 +4,7 @@ import {Component, Input, OnInit} from '@angular/core';
   selector: 'app-button-square',
   template: `
     <div class="button__background" [style.background-color]="color" [style.height.rem]="size" [style.width.rem]="size">
-      <img [src]='icon' class="button__icon" alt="button image">
+      <img [src]='icon' [style.height.px]="iconHeight ? iconHeight : 'auto'" class="button__icon" alt="button image">
     </div>
   `,
   styles: ['.button__icon { position: absolute; margin: 0; left: 50%; top: 50%; transform: translate(-50%, -50%);  }',
@@ -20,6 +20,9 @@ export class ButtonSquareComponent {
 
   @Input()
   icon: string;
+
+  @Input()
+  iconHeight: number;
 
   constructor() { }
 }
