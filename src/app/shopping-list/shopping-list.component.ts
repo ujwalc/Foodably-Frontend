15d7d020f5d8adf575df04d7e4ed84a7798ea110
SHASHANK REDDY MUPPIDI <sh375383@dal.ca>
2020-04-04 22:28:09 -0300

    Update shopping-list.component.ts

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -8,22 +8,18 @@ import { Subscription } from 'rxjs';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss']
 })
+
+//Makes a request to shopping-list service and fetch shoppinglist
 export class ShoppingListComponent implements OnInit, OnDestroy {
 
-  lat = '';//number = 44.6344
-  lng = '';//number = -63.5822
+  lat = '';
+  lng = '';
 
   shoppingListSub: Subscription;
   shoppingList: Array<Ingredient>;
   error: null;
 
-  location: Object;
-  // public shoppingList = [
-  //   {name:"Chicken",qty:"500gm"},
-  //   {name:"Oil",qty:"100ml"},
-  //   {name:"Chillies",qty:"10g"}
-  // ];
-
+//calls the api services
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
@@ -34,12 +30,6 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
     this.shoppingListSub.unsubscribe();
   }
 
-  // console.log("users array1", this.cookinglist);
-  // this.map.getlocation().subscribe(data => {
-  //   console.log(data);
-  //   this.lat = data.latitude;
-  //   this.lng=data.longitude;
-  // })
 
   getShoppingList() {
     this.shoppingListSub = this.shoppingListService.fetchUserCookingList().subscribe(
